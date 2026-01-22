1451b3376894c70261d1597cd6f72a5b153f759f
Azmekin <73522575+Azmekin@users.noreply.github.com>
2026-01-22 16:32:59 +0700

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,18 +1,10 @@
 # 1. Сначала устанавливаем приложения (старые скрипты)
 sudo ./gitlab-setup.sh
 sudo ./defect-dojo-setup.sh
-sudo ./gitlab-setup.sh
 sudo ./gitlab-public-user.sh
 # 2. Потом создаем сервисы (новые скрипты)
 sudo ./create-gitlab-service.sh
 sudo ./create-defectdojo-service.sh
-sudo ./create-security-stack-service.sh
-sudo ./setup-boot-service.sh
-
-# 3. Проверяем
-sudo systemctl status security-stack
-sudo systemctl status security-setup
+sudo ./repo-init.sh
 # 4. Генерируем ключ
 sudo setup-ctf-secret.sh
-# 5. Перезагружаем для проверки
-sudo reboot
